c6cd2e50e21bb178db6f9cfe068849b4b5905958
SoutPanhavon168 <Panhavon.sout@student.cadt.edu.kh>
2026-04-29 16:15:06 +0700

    update pass dto

--- lib/data/dtos/pass_dto.dart
+++ lib/data/dtos/pass_dto.dart
@@ -1,36 +1,32 @@
 import "../../model/pass/pass.dart";
 
 class PassDto {
-  final String id, title, duration, type;
+  final String id;
+  final String title;
   final double price;
+  final int durationDays;
 
-  PassDto({
+  const PassDto({
     required this.id,
     required this.title,
-    required this.duration,
-    required this.type,
     required this.price,
+    required this.durationDays,
   });
 
   factory PassDto.fromJson(String id, Map<String, dynamic> json) {
     return PassDto(
       id: id,
       title: json['title'] ?? '',
-      duration: json['duration'] ?? '',
-      type: json['type'] ?? 'day',
       price: (json['price'] as num).toDouble(),
+      durationDays: (json['durationDays'] as num?)?.toInt() ?? 1,
     );
   }
 
+  Map<String, dynamic> toJson() {
+    return {'title': title, 'price': price, 'durationDays': durationDays};
+  }
+
   Pass toDomain() {
-    return Pass(
-      id: id,
-      title: title,
-      price: price,
-      duration: duration,
-      type: type == 'monthly'
-          ? PassType.monthly
-          : (type == 'annual' ? PassType.annual : PassType.day),
-    );
+    return Pass(id: id, title: title, price: price, durationDays: durationDays);
   }
 }
